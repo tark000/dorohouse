32a75fe3158e6b51522857ae70d844f65349950e
tark000 <tark000@ukr.net>
2013-04-18 18:29:11 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dorohouse::Application.config.secret_token = 'e41b98736f7c1c25187f229f2dd2bc2df21dea14057fbc0e1c5e71e10695215473c669194e44144c15bbc6b25b80b53129c15e131517191972d6a7627b53112c'
